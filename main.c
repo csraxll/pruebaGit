dac23fb8c2f2a55078c416005e0c2ba178b8de57
Axel Lopez <csraxll@gmail.com>
2015-06-26 18:04:32 -0300

    Hello World borrado, ahora se suman dos numeros enteros.

--- main.c
+++ main.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
 int main(){
-  char i;
-  int x = 5;
-  int y = 7;
+  char i= 'a';
+  int x = 0;
+  int y = 0;
+
+  printf("Ingrese un número X para sumar\r\n");
+  scanf("%d", &x);
+
+  printf("Ingrese un número Y para sumar\r\n");
+  scanf("%d", &y);
   
-  printf("Hello World! El resultado es: %d", (x+y));
+  printf("El resultado es: %d \r\n", (x+y));
   
-  getchar();
+  while (i != '0'){
+    scanf("%c", &i);
+  }
   return 0;
 }
